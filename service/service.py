9b775ce761c62007e2eb41dc9804efbe87ec6e54
Tarjei Næss Skrede <tarjei.skrede@M-C02Y7599JGH6.bouvet.no>
2019-04-11 15:03:57 +0200

    Don't send every processed entity back, just a success status if ok, send current entity back if failing

--- service/service.py
+++ service/service.py
@@ -52,18 +52,15 @@ def send_to_list():
     """
     request_entities = request.get_json()
 
-    def generate(entities: list):
+    def post_entities(entities: list):
         ctx_auth = None
         token_acquired = False
         ctx = None
         values_to_send = None
         item_properties = None
         list_object = None
-        result = '['
-        for index, entity in enumerate(entities):
-            if index > 0:
-                result += ','
 
+        for index, entity in enumerate(entities):
             if ctx_auth is None:
                 ctx_auth = AuthenticationContext(URL)
 
@@ -117,18 +114,22 @@ def send_to_list():
                 except Exception as e:
                     logging.error("An exception has occurred: {}".format(e))
                     entity['status'] = "ERROR: An exception has occurred: {}".format(e)
-                    raise
+                    error_message = "An exception occurred during processing of current entity: {0} {1} ({2}".format(e.errno, e.strerror, json.dumps(entity))
+                    raise Exception(error_message)
             else:
                 error = ctx_auth.get_last_error()
                 logging.error(error)
                 entity['status'] = "ERROR: {}".format(error)
                 raise Exception(error)
 
-            result += str(json.dumps(entity))
-        result += ']'
-        return result
+        return True
 
-    return Response(generate(request_entities), mimetype='application/json')
+    status = post_entities(request_entities)
+
+    if status:
+        return Response(status=200, response="{'status': 'success'}", mimetype='application/json')
+    else:
+        return Response(status=500, response="An error occurred!")
 
 
 @APP.route('/get-from-list/<list_name>', methods=['GET'])
